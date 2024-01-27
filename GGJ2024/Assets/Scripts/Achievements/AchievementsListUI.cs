a421c0ad3817bbddcdde65c1b86954438f0aa0f9
Mikov Denis <denis.mikov.06@gmail.com>
2024-01-28 01:17:23 +0300

    feat(achievements): implemented window T binding

--- GGJ2024/Assets/Scripts/Achievements/AchievementsListUI.cs
+++ GGJ2024/Assets/Scripts/Achievements/AchievementsListUI.cs
@@ -3,29 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
-
-public class AchievementsListUI : MonoBehaviour
+namespace GGJ.Achievements
 {
-    [SerializeField] private AchievementsManager achievementsManager;
-    [SerializeField] private AchievementCard cardPrefab;
-    [SerializeField] private GridLayoutGroup viewport;
 
-    void Start()
+    public class AchievementsListUI : MonoBehaviour
     {
-        // HACK: made here for test.
-        achievementsManager.Load();
+        [SerializeField] private AchievementsManager achievementsManager;
+        [SerializeField] private AchievementCard cardPrefab;
+        [SerializeField] private GridLayoutGroup viewport;
 
-        // Initialize all cards (sort by completed, then by not secret).
-        foreach (var achievement in achievementsManager.Achievements.OrderBy(x => x switch
+        private void Start()
         {
-            { Completed: true } => 0,
-            { IsHidden: false } => 1,
-            _ => 2
-        }))
+            // HACK: made here for test.
+            achievementsManager.Load();
+
+            // Initialize all cards (sort by completed, then by not secret).
+            foreach (var achievement in achievementsManager.Achievements.OrderBy(x => x switch
+            {
+                { Completed: true } => 0,
+                { IsHidden: false } => 1,
+                _ => 2
+            }))
+            {
+                var card = Instantiate(cardPrefab, viewport.transform);
+                card.SetAchievementInfo(achievement);
+            }
+
+            gameObject.SetActive(false);
+        }
+
+        public void OpenCloseWindow(InputAction.CallbackContext callbackContext)
         {
-            var card = Instantiate(cardPrefab, viewport.transform);
-            card.SetAchievementInfo(achievement);
+            if (callbackContext.canceled)
+                return;
+
+            gameObject.SetActive(!gameObject.activeInHierarchy);
         }
     }
 }
