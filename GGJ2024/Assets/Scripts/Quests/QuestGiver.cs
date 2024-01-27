6fbe0b41aba3da60da28d38fb6a98fe714859fef
Mikov Denis <denis.mikov.06@gmail.com>
2024-01-28 02:27:03 +0300

    feat(quests): removed extra code

--- GGJ2024/Assets/Scripts/Quests/QuestGiver.cs
+++ GGJ2024/Assets/Scripts/Quests/QuestGiver.cs
@@ -4,19 +4,6 @@ namespace GGJ.Quests
 {
     public class QuestGiver : MonoBehaviour
     {
-        [SerializeField] private Quest quest;
-
-        public void TakeQuest()
-        {
-            
-        }
-
-        public void PassQuest(Quest questToPass)
-        {
-            if (questToPass.QuestInfo == quest.QuestInfo && questToPass.IsCompleted)
-            {
-                
-            }
-        }
+        
     }
 }
