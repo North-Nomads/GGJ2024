9fc7384d90c35e6b16b4ed732aaf8f00a2622bd1
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-06 20:19:36 +0500

    feat(Asset): added character prefab path

--- GGJ2024/Assets/Scripts/Infrastructure/AssetManagement/AssetPath.cs
+++ GGJ2024/Assets/Scripts/Infrastructure/AssetManagement/AssetPath.cs
@@ -2,7 +2,7 @@
 {
     public static class AssetPath
     {
-        public const string CharacterPath = "";
+        public const string CharacterPath = "Assets/Resources/Prefabs/Player/Player 1";
         public const string HudPath = "";
     }
 }
