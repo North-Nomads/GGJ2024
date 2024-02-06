6472c1985649584ee5927edc9442876a9ca9220e
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-06 20:00:50 +0500

    feat(Game): game bootstrapper

---
+++ GGJ2024/Assets/Scripts/Infrastructure/Game.cs
@@ -0,0 +1,17 @@
+﻿using GGJ.Infrastructure.Services;
+using GGJ.Infrastructure.States;
+
+namespace GGJ.Infrastructure
+{
+    public class Game
+    {
+        private readonly GameStateMachine _stateMachine;
+        private readonly SceneLoader _sceneLoader;
+
+        public Game(ICoroutineRunner coroutineRunner)
+        {
+            _sceneLoader = new SceneLoader(coroutineRunner);
+            _stateMachine = new GameStateMachine(_sceneLoader, AllServices.Container);
+        }
+    }
+}
---
+++ GGJ2024/Assets/Scripts/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace GGJ.Infrastructure
+{
+    public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
+    {
+        private Game _game;
+
+        private void Awake()
+        {
+            _game = new Game(this);
+        }
+    }
+}
