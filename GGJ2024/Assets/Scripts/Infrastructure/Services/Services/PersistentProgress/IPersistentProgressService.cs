8456c33fdab2d3ce6c96adeb435755d1014e470d
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-06 19:58:39 +0500

    feat(Data): implemented progress service

---
+++ GGJ2024/Assets/Scripts/Infrastructure/Services/Services/PersistentProgress/IPersistentProgressService.cs
@@ -0,0 +1,9 @@
+﻿using GGJ.Data;
+
+namespace GGJ.Infrastructure.Services.Services.PersistentProgress
+{
+    public interface IPersistentProgressService : IService
+    {
+        PlayerProgress Progress { get; set; }
+    }
+}
