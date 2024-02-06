37e37508b6ebd5fe5476abd5620437e22fdb280e
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-06 19:58:05 +0500

    feat(Asset): implemented asset provider

---
+++ GGJ2024/Assets/Scripts/Infrastructure/AssetManagement/AssetPath.cs
@@ -0,0 +1,8 @@
+﻿namespace GGJ.Infrastructure.AssetManagement
+{
+    public static class AssetPath
+    {
+        public const string CharacterPath = "";
+        public const string HudPath = "";
+    }
+}
---
+++ GGJ2024/Assets/Scripts/Infrastructure/AssetManagement/IAssetProvider.cs
@@ -0,0 +1,11 @@
+﻿using GGJ.Infrastructure.Services;
+using UnityEngine;
+
+namespace GGJ.Infrastructure.AssetManagement
+{
+    public interface IAssetProvider : IService
+    {
+        GameObject Instantiate(string path);
+        GameObject Instantiate(string path, Vector3 at);
+    }
+}
