e90f939c2c06b7041c7b56a0f1fda2a810c4d50c
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-13 00:02:24 +0500

    feat(State): quest givers initialization

--- GGJ2024/Assets/Scripts/Infrastructure/States/LoadLevelState.cs
+++ GGJ2024/Assets/Scripts/Infrastructure/States/LoadLevelState.cs
@@ -1,8 +1,13 @@
-﻿using GGJ.Infrastructure.Factories;
+﻿using System;
+using GGJ.Dialogs;
+using GGJ.Infrastructure.AssetManagement;
+using GGJ.Infrastructure.Factories;
 using GGJ.Infrastructure.Services.Services.PersistentProgress;
 using GGJ.Infrastructure.Services.Services.SaveLoad;
 using GGJ.Infrastructure.States.Interfaces;
+using GGJ.Quests;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace GGJ.Infrastructure.States
 {
@@ -14,14 +19,16 @@ namespace GGJ.Infrastructure.States
         private readonly SceneLoader _sceneLoader;
         private readonly IPersistentProgressService _progressService;
         private readonly IGameFactory _gameFactory;
+        private readonly IAssetProvider _assetProvider;
 
 
-        public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader, IPersistentProgressService progressService, IGameFactory gameFactory)
+        public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader, IPersistentProgressService progressService, IGameFactory gameFactory, IAssetProvider assetProvider)
         {
             _stateMachine = stateMachine;
             _sceneLoader = sceneLoader;
             _progressService = progressService;
             _gameFactory = gameFactory;
+            _assetProvider = assetProvider;
         }
 
         public void Enter<TPayload>(TPayload payload) => 
@@ -40,8 +47,18 @@ namespace GGJ.Infrastructure.States
         private void InitGameWorld()
         {
             _gameFactory.CleanUp();
-            _gameFactory.CreateCharacter(GameObject.FindGameObjectWithTag(InitialCharacterPointTag).transform.position);
-            _gameFactory.CreateHud();
+            GameObject character = _gameFactory.CreateCharacter(GameObject.FindGameObjectWithTag(InitialCharacterPointTag).transform.position).transform.GetChild(0).gameObject;
+            //_gameFactory.CreateHud();
+
+            InitQuestGivers(character);
+        }
+
+        private void InitQuestGivers(GameObject character)
+        {
+            QuestGiverPrefab[] questGivers =
+                Object.FindObjectsByType<QuestGiverPrefab>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            
+            Array.ForEach(questGivers, e => e.Construct(character));
         }
 
         private void InformProgressReaders()
