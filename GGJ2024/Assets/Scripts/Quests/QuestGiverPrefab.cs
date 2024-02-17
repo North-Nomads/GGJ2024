e051ee7a3184b9928a123ccb396204bd4c22cb7d
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-17 16:14:02 +0500

    feat(Quest): quest giver events handling

--- GGJ2024/Assets/Scripts/Quests/QuestGiverPrefab.cs
+++ GGJ2024/Assets/Scripts/Quests/QuestGiverPrefab.cs
@@ -13,19 +13,34 @@ namespace GGJ.Quests
     /// </summary>
     public class QuestGiverPrefab : MonoBehaviour, ICoroutineRunner
     {
+        private const string PlayerTag = "Player";
+        
+        private static bool _isDialogContinues;
+
         [SerializeField] private QuestGiverInfo questGiverInfo;
         [SerializeField] private DialogView dialogView;
+        [SerializeField] private Canvas talkHint;
+        [SerializeField] private TriggerObserver triggerObserver;
 
-        private static bool _isDialogContinues = false;
-        
         private QuestManager _questManager;
-        private IDialog _questDialog;
         private QuestInfo _currentQuest;
+        private IDialog _questDialog;
+
+        private bool _isPlayerInTalkRadius;
 
         private bool IsPlayerAlreadyHaveQuest => _questManager.CurrentQuest != null;
         private bool HaveAvailableQuests => questGiverInfo.Quests.Any(quest => quest.Id > _questManager.LastCompletedQuest.Id);
         private bool IsPlayerCompletedQuest => _questManager.IsCurrentQuestCompleted && questGiverInfo.GetQuestWithID(_questManager.CurrentQuest.Id);
-        
+        private bool IsPlayerInTalkRadius
+        {
+            get => _isPlayerInTalkRadius;
+            set
+            {
+                _isPlayerInTalkRadius = value;
+                ToggleTalkHint(_isPlayerInTalkRadius);
+            }
+        }
+
         public void Construct(GameObject character)
         {
             if (character.TryGetComponent(out QuestManager questManager)) 
@@ -33,56 +48,96 @@ namespace GGJ.Quests
 
             if (character.TryGetComponent(out DialogInputHandler dialogInputHandler))
                 dialogInputHandler.DialogStartButtonPressed += OnDialogStartButtonPressed;
-
+            
             _questDialog = new QuestDialog();
             _questDialog.Initialize(character, dialogView);
             _questDialog.DialogStarted += OnQuestDialogStarted;
             _questDialog.DialogEnded += OnQuestDialogEnded;
         }
 
+        private void OnEnable()
+        {
+            triggerObserver.TriggerEntered += TriggerEntered;
+            triggerObserver.TriggerExited += TriggerExited;
+        }
+
+        private void OnDisable()
+        {
+            triggerObserver.TriggerEntered -= TriggerEntered;
+            triggerObserver.TriggerExited -= TriggerExited;
+        }
+
         private void OnDialogStartButtonPressed()
         {
-            if (_isDialogContinues) return;
+            if (!_isPlayerInTalkRadius || _isDialogContinues) return;
             
-            if (IsPlayerAlreadyHaveQuest || !HaveAvailableQuests)
-                ShowBusyPhrase();
-            else if (IsPlayerCompletedQuest)
+            if (IsPlayerCompletedQuest)
                 CompleteQuest();
+            else if (IsPlayerAlreadyHaveQuest || !HaveAvailableQuests)
+                ShowBusyPhrase();
             else
                 StartQuest();
         }
 
-        private void ShowBusyPhrase() => 
-            _questDialog.ShowPhrase(this, questGiverInfo.name, questGiverInfo.BusyPhrase);
+        private void ShowBusyPhrase() =>
+            StartCoroutine(_questDialog.ShowDialog(this, questGiverInfo.name, new [] {questGiverInfo.BusyPhrase}, DialogType.Busy));
 
         private void ShowQuestDialog() => 
-            StartCoroutine(_questDialog.ShowDialog(this, questGiverInfo.name, _currentQuest.MonologSpeeches));
+            StartCoroutine(_questDialog.ShowDialog(this, questGiverInfo.name, _currentQuest.MonologSpeeches, DialogType.Quest));
 
         private void ShowCompletedPhrase() => 
-            _questDialog.ShowPhrase(this, questGiverInfo.name, _currentQuest.FinishText);
+            StartCoroutine(_questDialog.ShowDialog(this, questGiverInfo.name, new[] {_currentQuest.QuestGiverFinishText}, DialogType.Complete));
 
         private void StartQuest()
         {
             if (questGiverInfo.TryGetNextQuest(_questManager.LastCompletedQuest, out _currentQuest))
-            {
-                _questManager.CurrentQuest = _currentQuest;
                 ShowQuestDialog();
-            }
             else
                 ShowBusyPhrase();
         }
 
         private void CompleteQuest()
         {
+            ShowCompletedPhrase();
+            
             _questManager.SubmitCurrentQuest();
             
             _currentQuest = null;
-            
-            ShowCompletedPhrase();
         }
 
-        private void OnQuestDialogStarted() => _isDialogContinues = true;
+        private void OnQuestDialogStarted(DialogType dialogType)
+        {
+            _isDialogContinues = true;
+            talkHint.gameObject.SetActive(false);
+        }
 
-        private void OnQuestDialogEnded() => _isDialogContinues = false;
+
+        private void OnQuestDialogEnded(DialogType dialogType)
+        {
+            _isDialogContinues = false;
+            talkHint.gameObject.SetActive(true);
+            
+            if (dialogType == DialogType.Quest)
+                _questManager.CurrentQuest = _currentQuest;
+        }
+
+        private void TriggerEntered(Collider other)
+        {
+            Debug.Log("Trigger entered");
+            if (other.CompareTag(PlayerTag))
+            {
+                Debug.Log("Compared Tag");
+                IsPlayerInTalkRadius = true;
+            }
+        }
+
+        private void TriggerExited(Collider other)
+        {
+            if (other.CompareTag(PlayerTag))
+                IsPlayerInTalkRadius = false;
+        }
+
+        private void ToggleTalkHint(bool value) => 
+            talkHint.gameObject.SetActive(value);
     }
 }
