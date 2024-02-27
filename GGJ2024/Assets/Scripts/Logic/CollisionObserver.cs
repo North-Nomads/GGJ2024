dfa5ffb1fb6d201abd865d18ac76c91c55f4a52b
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-27 22:40:36 +0500

    feat(Logic): implemented Collision observer

---
+++ GGJ2024/Assets/Scripts/Logic/CollisionObserver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Logic
+{
+    [RequireComponent(typeof(Collider))]
+    public class CollisionObserver : MonoBehaviour
+    {
+        public event Action<Collision> CollisionEntered;
+        public event Action<Collision> CollisionExited;
+
+        private void OnCollisionEnter(Collision other) => CollisionEntered?.Invoke(other);
+        private void OnCollisionExit(Collision other) => CollisionExited?.Invoke(other);
+    }
+}
