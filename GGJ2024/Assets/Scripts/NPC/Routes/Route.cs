dba76052583aa69f489a28445bcb8cd0c3e48572
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-27 22:41:50 +0500

    feat(NPC): implemented routes logic

---
+++ GGJ2024/Assets/Scripts/NPC/Routes/Route.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace NPC.Routes
+{
+    public class Route : MonoBehaviour
+    {
+        [SerializeField] private List<RoutePoint> points;
+        
+        public RoutePoint RootPoint { get; private set; }
+        public RoutePoint DestinationPoint { get; private set; }
+
+        public event EventHandler<bool> RootPointCameraVisibleChanged;
+
+        public void Initialize()
+        {
+            if (points.Count == 0)
+                throw new NullReferenceException("Route haven't any point");
+
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                points[i].NextPoint = points[i + 1];
+                
+                if (i == 0) continue;
+                
+                points[i].PreviousPoint = points[i - 1];
+            }
+
+            RootPoint = points.First(point => point.IsRoot);
+            DestinationPoint = points.First(point => point.IsLast);
+
+            RootPoint.VisibleChanged += OnRootPointCameraVisibleChanged;
+        }
+
+        private void OnRootPointCameraVisibleChanged(object sender, bool isVisible)
+        {
+            if (sender as RoutePoint == RootPoint) 
+                RootPointCameraVisibleChanged?.Invoke(this, isVisible);
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            if (!Application.isPlaying) return;
+            if (RootPoint == null) return;
+            
+            Gizmos.color = Color.white;
+            DrawPointLines();
+        }
+        
+        private void DrawPointLines()
+        {
+            RoutePoint currentPoint = RootPoint;
+
+            while (!currentPoint.IsLast)
+            {
+                Gizmos.DrawLine(currentPoint.transform.position, currentPoint.NextPoint.transform.position);
+                currentPoint = currentPoint.NextPoint;
+            }
+        }
+#endif
+    }
+}
---
+++ GGJ2024/Assets/Scripts/NPC/Routes/RoutePoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using GGJ.Quests;
+using JetBrains.Annotations;
+using Logic;
+using UnityEngine;
+
+namespace NPC.Routes
+{
+    [RequireComponent(typeof(Collider))]
+    public class RoutePoint : MonoBehaviour
+    {
+        [SerializeField] private TriggerObserver triggerObserver;
+        
+        public TriggerObserver TriggerObserver => triggerObserver;
+        [CanBeNull] public RoutePoint NextPoint { get; set; }
+        [CanBeNull] public RoutePoint PreviousPoint { get; set; }
+
+        public bool IsRoot => PreviousPoint is null;
+        public bool IsLast => NextPoint is null;
+
+        public event EventHandler<bool> VisibleChanged;
+
+        private void OnBecameVisible()
+        {
+            if (IsRoot) 
+                VisibleChanged?.Invoke(this, true);
+        }
+
+        private void OnBecameInvisible()
+        {
+            if (IsRoot) 
+                VisibleChanged?.Invoke(this, false);
+        }
+    }
+}
