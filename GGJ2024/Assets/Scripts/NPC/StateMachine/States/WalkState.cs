5a8de8870798d10cc741cbf558c6ce4b38acfa48
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-02-28 22:28:39 +0500

    feat(NPC): added walk state logic

--- GGJ2024/Assets/Scripts/NPC/StateMachine/States/WalkState.cs
+++ GGJ2024/Assets/Scripts/NPC/StateMachine/States/WalkState.cs
@@ -1,17 +1,65 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using GGJ.Infrastructure;
+using NPC.Components;
+using NPC.Settings;
+using UnityEngine;
 
 namespace NPC.StateMachine.States
 {
     public sealed class WalkState : BaseState
     {
-        public WalkState(NpcStateMachine stateMachine) : base(stateMachine) { }
+        private readonly RouteProvider _routeProvider;
+        private readonly Rigidbody _rigidbody;
+        private readonly NpcSettings _settings;
+
+        private Coroutine _walkRoutine;
+
+        public WalkState(NpcStateMachine stateMachine, ICoroutineStopper coroutineStopper, RouteProvider routeProvider, Rigidbody rigidbody, NpcSettings settings) : base(stateMachine, coroutineStopper)
+        {
+            _routeProvider = routeProvider;
+            _rigidbody = rigidbody;
+            _settings = settings;
+
+            //routeProvider.DestinationPointReached += StopRouteWalk;
+        }
 
         public override void InitializeTransitions()
         {
-            Transitions.AddRange(new[]
+            /*Transitions.AddRange(new[]
             {
                 new Transition(StateMachine.GetState<IdleState>(), () => true),
-            });
+            });*/
+        }
+
+        public override void Tick()
+        {
+            if (_routeProvider.Route != null && _walkRoutine == null) 
+                _walkRoutine = CoroutineStopper.StartCoroutine(StartRouteWalk());
+        }
+
+        public override void Exit() => StopRouteWalk();
+
+        private IEnumerator StartRouteWalk()
+        {
+            while (true)
+            {
+                Vector3 nextPoint =
+                    _rigidbody.transform.InverseTransformPoint(_routeProvider.CurrentRoutePoint.transform.position) * 
+                    (Time.fixedDeltaTime * _settings.MaxWalkSpeed);
+                
+                _rigidbody.MovePosition(_rigidbody.position + nextPoint);
+                
+                yield return new WaitForFixedUpdate();
+            }
+        }
+
+        private void StopRouteWalk()
+        {
+            if (_walkRoutine != null)
+            {
+                CoroutineStopper.StopCoroutine(_walkRoutine);
+                _walkRoutine = null;
+            }
         }
     }
 }
