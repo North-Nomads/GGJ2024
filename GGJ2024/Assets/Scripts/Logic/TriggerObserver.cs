c17be2e925eb1bfac6f069099a8b56d89f0468ff
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-03-01 00:28:05 +0500

    feat(Logic): trigger observer features

--- GGJ2024/Assets/Scripts/Logic/TriggerObserver.cs
+++ GGJ2024/Assets/Scripts/Logic/TriggerObserver.cs
@@ -6,11 +6,18 @@ namespace Logic
     [RequireComponent(typeof(Collider))]
     public class TriggerObserver : MonoBehaviour
     {
+        public Collider Collider { get; private set; }
+        
         public event Action<Collider> TriggerEntered;
         public event Action<Collider> TriggerExited;
+        public event Action<Collider> TriggerStayed;
+
+        private void Awake() => Collider = GetComponent<Collider>();
 
         private void OnTriggerEnter(Collider other) => TriggerEntered?.Invoke(other);
 
         private void OnTriggerExit(Collider other) => TriggerExited?.Invoke(other);
+
+        private void OnTriggerStay(Collider other) => TriggerStayed?.Invoke(other);
     }
 }
