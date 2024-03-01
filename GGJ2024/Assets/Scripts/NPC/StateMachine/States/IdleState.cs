a3a7a775602d8e9d0f904abdc532d89b6f5f40e1
Sergei Shalamow <serezha.schalamow@yandex.ru>
2024-03-02 01:48:38 +0500

    refactor(NPC): changed walking logic for navmeshagent

--- GGJ2024/Assets/Scripts/NPC/StateMachine/States/IdleState.cs
+++ GGJ2024/Assets/Scripts/NPC/StateMachine/States/IdleState.cs
@@ -1,17 +1,26 @@
 ﻿using GGJ.Infrastructure;
+using NPC.Components;
+using NPC.Settings;
 
 namespace NPC.StateMachine.States
 {
     public sealed class IdleState : BaseState
     {
-        public IdleState(NpcStateMachine stateMachine, ICoroutineRunner coroutineRunner) : base(stateMachine, coroutineRunner) { }
+        private readonly NpcSettings _settings;
+        private readonly RouteProvider _routeProvider;
+        
+        public IdleState(NpcStateMachine stateMachine, ICoroutineRunner coroutineRunner, NpcSettings settings, RouteProvider routeProvider) : base(stateMachine, coroutineRunner)
+        {
+            _settings = settings;
+            _routeProvider = routeProvider;
+        }
         
         public override void InitializeTransitions()
         {
-            /*Transitions.AddRange(new[]
+            Transitions.AddRange(new[]
             {
-                new Transition(StateMachine.GetState<WalkState>(), () => true),
-            });*/
+                new Transition(StateMachine.GetState<WalkState>(), () => !_settings.IsShouldStay && _routeProvider.Route != null),
+            });
         }
     }
 }
--- GGJ2024/Assets/Scripts/NPC/StateMachine/States/WalkState.cs
+++ GGJ2024/Assets/Scripts/NPC/StateMachine/States/WalkState.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using GGJ.Infrastructure;
 using NPC.Components;
 using NPC.Settings;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 namespace NPC.StateMachine.States
 {
@@ -10,24 +13,29 @@ namespace NPC.StateMachine.States
     {
         private readonly RouteProvider _routeProvider;
         private readonly Rigidbody _rigidbody;
+        private readonly NavMeshAgent _navMeshAgent;
         private readonly NpcSettings _settings;
 
         private Coroutine _walkRoutine;
 
-        public WalkState(NpcStateMachine stateMachine, ICoroutineRunner coroutineRunner, RouteProvider routeProvider, Rigidbody rigidbody, NpcSettings settings) : base(stateMachine, coroutineRunner)
+        public bool IsNeedToRun => _settings.CanRun && Random.Range(0f, 1f) >= 0.5f;
+
+        public WalkState(NpcStateMachine stateMachine, ICoroutineRunner coroutineRunner, RouteProvider routeProvider, Rigidbody rigidbody, NavMeshAgent navMeshAgent, NpcSettings settings) : base(stateMachine, coroutineRunner)
         {
             _routeProvider = routeProvider;
             _rigidbody = rigidbody;
+            _navMeshAgent = navMeshAgent;
             _settings = settings;
 
-            //routeProvider.DestinationPointReached += StopRouteWalk;
+            routeProvider.DestinationPointReached += StopRouteWalk;
+            routeProvider.CurrentPointChanged += OnCurrentPointChanged;
         }
 
         public override void InitializeTransitions()
         {
             /*Transitions.AddRange(new[]
             {
-                new Transition(StateMachine.GetState<IdleState>(), () => true),
+                new Transition(StateMachine.GetState<IdleState>(), () => ),
             });*/
         }
 
@@ -37,23 +45,26 @@ namespace NPC.StateMachine.States
                 _walkRoutine = CoroutineRunner.StartCoroutine(StartRouteWalk());
         }
 
-        public override void Exit() => StopRouteWalk();
+        public override void Exit() => StopRouteWalk(this, null);
 
+        private void OnCurrentPointChanged()
+        {
+            _navMeshAgent.speed = IsNeedToRun ? _settings.MaxRunSpeed : _settings.MaxWalkSpeed;
+            _navMeshAgent.destination = _routeProvider.CurrentRoutePoint.transform.position;
+        }
+        
         private IEnumerator StartRouteWalk()
         {
             while (true)
             {
-                Vector3 nextPoint =
-                    _rigidbody.transform.InverseTransformPoint(_routeProvider.CurrentRoutePoint.transform.position) * 
-                    (Time.fixedDeltaTime * _settings.MaxWalkSpeed);
+                if (_navMeshAgent.velocity.sqrMagnitude > Mathf.Epsilon) 
+                    _rigidbody.transform.rotation = Quaternion.LookRotation(_navMeshAgent.velocity.normalized);
                 
-                _rigidbody.MovePosition(_rigidbody.position + nextPoint);
-                
-                yield return new WaitForFixedUpdate();
+                yield return new WaitForEndOfFrame();
             }
         }
 
-        private void StopRouteWalk()
+        private void StopRouteWalk(object sender, EventArgs args)
         {
             if (_walkRoutine != null)
             {
